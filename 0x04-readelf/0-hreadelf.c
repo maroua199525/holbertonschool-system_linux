afa9acbd4bd395d46510fccabf7ecd1b03449a21
maroua199525 <2646@holbertonschool.com>
2022-01-27 19:50:45 +0100

    add first task

---
+++ 0x04-readelf/0-hreadelf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * main - displays the information contained in the ELF file header of an ELF file
+ * @argc: the length of argc
+ * @argv: argements
+ * @env:  the environment
+ * Return: -1 in fail and 0 in success
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-h", "", NULL};
+
+	(void)argc;
+	args[3] = argv[1];
+	if (execve("/usr/bin/readelf", args, env) == -1)
+	{
+		perror("execv");
+		return (-1);
+	}
+	return (0);
+}

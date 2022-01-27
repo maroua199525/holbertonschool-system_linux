fa348bfd1c9cbada299ddfb69415eb26da33ca1e
maroua199525 <2646@holbertonschool.com>
2022-01-27 20:12:17 +0100

    add final task

---
+++ 0x04-readelf/100-hreadelf.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+/**
+ * main - displays the information contained in the ELF program headers
+ * @argc: arguments conter
+ * @argv: argv (argument vector), is an array of pointers to arrays of objects
+ * @env:  environment
+ * Return: -1 in fail and 0 in success
+ */
+int main(int argc, char **argv, char **env)
+{
+	char *args[] = {"/usr/bin/readelf", "-W", "-s", "", NULL};
+
+	(void)argc;
+	args[3] = argv[1];
+	if (execve("/usr/bin/readelf", args, env) == -1)
+	{
+		perror("execv");
+		return (-1);
+	}
+	return (0);
+}

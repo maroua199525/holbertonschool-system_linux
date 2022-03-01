39a01437228b9a662362531956d2fa3c75021ad9
maroua199525 <2646@holbertonschool.com>
2022-03-01 11:51:10 +0100

    fix code

--- 0x05-libasm/8-strcasecmp.asm
+++ 0x05-libasm/8-strcasecmp.asm
@@ -1,60 +1,68 @@
 BITS 64
 
-global asm_strcasecmp
+global asm_strcasecmp			; export to the gcc link
 
-section .text
+
 asm_strcasecmp:
-    push    rbp
-    push    rcx
-    mov     rbp, rsp
-    xor     rcx, rcx
+	push rbp					; push the base
+	mov rbp, rsp				; start new base
 
-loop_1:
-    mov   r8,  [rdi + rcx]
-    mov   r9,  [rsi + rcx]
-    cmp     r8b, 0
-    je      end
-    cmp     r9b, 0
-    je      end
+	mov R8, 0h					; set return to 0
+	mov R9, 0h					; set counter to 0
 
-compare_upper_first:
-    cmp     r8b, 'A'
-    jae     compare_upper_z
-    jmp     compare_upper_sec
+asm_loop:
+	mov al, [rdi + R9]			; store the char of first argument + counter in al
+	mov bl, [rsi + R9]			; store the char of second argument + counter in ab
 
-compare_upper_z:
-    cmp     r8b, 'Z'
-    jbe     to_lower_first
-    jmp     compare_upper_sec
+	cmp al, 65
+	jl nop_cap
+	cmp al, 90
+	jg nop_cap
 
-to_lower_first:
-    xor     r8b, 32
+	add al, 32
 
-compare_upper_sec:
-    cmp     r9b, 'A'
-    jae     compare_z_sec
-    jmp     loop_2
+compare:
+	cmp al, bl					; compare the char + counter of first and second argument
+	je equal_char				; if equal jump to equal_char
 
-compare_z_sec:
-    cmp     r9b, 'Z'
-    jbe     to_lower_sec
-    jmp     loop_2
+	movzx R8, al
+	movzx rcx, bl
+	sub R8, rcx
 
-to_lower_sec:
-    xor     r9b, 32
+	test R8, R8
+	jnz asm_end
+	test rcx, rcx
+	jz asm_end
 
-loop_2:
-    mov     dl, r9b
-    cmp     r8b, dl
-    jne     end
-    inc     rcx
-    jmp     loop_1
+	jmp asm_end
 
-end:
-    movzx     rax, BYTE [rdi + rcx]
-    movzx     r9, BYTE [rsi + rcx]
-    sub       rax, r9
-    mov       rsp, rbp
-    pop       rcx
-    pop       rbp
-    ret
+nop_cap:
+	cmp bl, 65
+	jl compare
+	cmp bl, 90
+	jg compare
+	add bl, 32
+	jmp compare
+
+equal_char:
+	cmp al, 0h					; compare al to Null
+	mov R8, 0					; set return to 0 if true
+	je asm_end					; jmp to asm_end
+
+	inc R9						; increace counter
+	jmp asm_loop				; loop again
+
+asm_end:
+	cmp R8, 32
+	je new_end
+	mov rax, R8					; pass the counter to the return register
+	mov rsp, rbp				; return to old base
+	pop rbp						; pop to the call base
+	ret
+
+new_end:
+	mov R8, 0h
+	mov rax, R8					; pass the counter to the return register
+	mov rsp, rbp				; return to old base
+	pop rbp						; pop to the call base
+	ret

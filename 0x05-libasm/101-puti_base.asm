e938c6fcc48ecdfe66f72cc231cead3488b74eb0
maroua199525 <2646@holbertonschool.com>
2022-03-03 14:14:24 +0100

    fix code

--- 0x05-libasm/101-puti_base.asm
+++ 0x05-libasm/101-puti_base.asm
@@ -1,112 +1,128 @@
 BITS 64
+	global asm_puti_base
+	extern asm_putc
+	extern asm_strlen
+	section .text
 
-section .bss
-
-    buf     resb        34      ; Charcter array with 11 bytes
-
-extern asm_putc
-extern asm_strlen
-global asm_puti_base
-
-section .text
-
-    ;; rax(rdi, rsi, rdx, r10, r8, r9)
-    ;; size_t asm_puti_base(int n, const char *base);
-    ;; @edi: n
-    ;; @rsi: base
 asm_puti_base:
+	push rbp
+	mov rbp, rsp
+	push r15
+ 	push r14
+	push r13
+	push r12
 
-    push rbp
-    mov rbp, rsp
-    push r8
-    push r9
-    push r10
-    ;; push r11
-    push rcx
-    push rsi
-    push rbx
+	xor r15, r15
+	xor r14, r14	; r14 is our char counter
+	movsx r15, edi	; r15 holds our integer n
+	;mov rsi, r12
+	xor rax, rax
 
-    mov r8, buf                 ; @r8: is character buffer
-    mov r9, rsi                 ; @r9: char array of characters
-    mov r10, 32                 ; Position in buffer mov BYTE [r8 + r10], 65
-    mov BYTE [r8 + 33], 0       ; Put null byte at the end of array
-    movsx rdi, edi              ; Fill rdi's 32 bits with zeros, good for handling overflows
-    xor sil, sil                ; Sign flag
+	; print negative first then make positive
+	cmp r15, 0
+	jge after_negative
+	imul r15, -1
+	mov r13, 1
+	mov rdi, 45
+	push rbx
+	push rsi
+	call asm_putc
+	pop rsi
+	pop rbx
 
-    mov rcx, rdi
-    mov rdi, r9
-    call asm_strlen
-    cmp eax, 2
-    jl print
-    mov rbx, rax                  ; Base
-    mov rdi, rcx
+after_negative:
+	; get base string length;
+	mov rdi, rsi
+	call asm_strlen
+	mov rbx, rax	; case 0?
+	cmp rbx, 1
+	jnz test_zero
+	; base is 1
+	mov r14, r15
+	loop_unary:
+	test r15, r15
+	jz done_loop_unary
+	xor rax, rax
+	mov al, BYTE [rsi]
+	push rax
+	dec r15
+	jmp loop_unary
+	done_loop_unary:
+	xor rax, rax
+	jmp while_n
 
-    xor rcx, rcx                ; Number of characters
-    cmp rdi, 0
-    jl negative
+test_zero:
+	; handle case where n == 0
+	test r15, r15
+	jnz while_n
+	movzx rdi, BYTE [rsi]
+	call asm_putc
+	mov r13, 1
 
-continue:
+while_n:
+	test rbx, rbx
+	jz while_n_nobase
+	; RAX starts as dividend becomes quotient
+	; RDX receives remainder
+	xor rdx, rdx
+	test r15, r15
+	jz done_while_n
+	mov rax, r15
+	;mov rbx, 10
+	div rbx
+	; print remainder in RDX
+	mov r15, rax
+	xor rax, rax
+	mov al, BYTE [rsi + rdx]
+	push rax
+	inc r14
+	jmp while_n
+done_while_n:
+	jmp done_both_while
 
-    mov rax, rdi
+while_n_nobase:
+	; RAX starts as dividend becomes quotient
+	; RDX receives remainder
+	xor rdx, rdx
+	test r15, r15
+	jz done_while_n_nobase
+	mov rax, r15
+	mov rbx, 10
+	div rbx
+	; print remainder in RDX
+	mov r15, rax
+	xor rax, rax
+	mov rax, rdx
+	add rax, 48
+	push rax
+	inc r14
+	jmp while_n_nobase
+done_while_n_nobase:
 
-do:
-
-    xor rdx, rdx                ; to zero-extend RAX into RDX:RAX.
-    div rbx                     ; @rdx will contain remainder, @rax will contain rdi/10
-    mov dl, BYTE [r9 + rdx]     ; use base tabel to choose right symbol
-    mov BYTE [r8 + r10], dl
-    inc rcx
-
-while:
-
-    test rax, rax
-    jz check_sign
-    dec r10
-    jmp do
-
-negative:
-
-    imul rdi, -1
-    mov sil, -1
-    jmp continue
-
-check_sign:
-
-    cmp sil, -1
-    jne print
-    dec r10
-    mov BYTE [r8 + r10], '-'     ; Put '-' sign
-    inc rcx
-
-print:                          ; Better to call write function once, but requirements don't allow it
-
-    lea r8, [r8 + r10]
-    xor r9, r9                  ; Reuse for indexing @r8
-
+done_both_while:
+	mov r15, r14;	move counter
 print_loop:
+	test r15, r15
+	jz done_print_loop
+	pop rdi
+	after_pop:
+	;add rdi, 48
+	call asm_putc
+	dec r15
+	jmp print_loop
 
-    mov dl, BYTE [r8 + r9]
-    test dl, dl
-    jz end
-    movzx rdi, dl
-    call asm_putc
-    inc r9
-    jmp print_loop
+done_print_loop:
+
+	cmp r13, 1
+	jne end
+	inc r14
 
 end:
-
-    mov rax, r9
-
-    pop rcx                     ; Attention!
-    pop rbx
-    pop rsi
-    pop rcx
-    ;; pop r11
-    pop r10
-    pop r9
-    pop r8
-
-    mov rsp, rbp
-    pop rbp
-
-    ret
+	mov rax, r14
+	pop r12
+	pop r13
+	pop r14
+	pop r15
+	mov rsp, rbp
+	pop rbp
+	ret

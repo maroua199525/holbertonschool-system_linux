6cd80655f4f08eb1d4448ac442cf284ef2854289
maroua199525 <2646@holbertonschool.com>
2022-04-14 13:45:18 +0100

    sooo tireeeed

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -9,29 +9,29 @@
 void print_python_int(PyObject *p)
 {
 	unsigned long x = 0;
-    digit *size;
+		digit *arr;
 	Py_ssize_t size, i, n;
 
 
 	if (PyLong_Check(p))
 	{
-        size = ((PyLongObject *)(p))->ob_digit;
-	    size = ((PyVarObject *)(p))->ob_size;
-	    n = abs(size);
-        if (n > 3 || (n == 3 && size[2] > 15))
-	    {
-		    printf("C unsigned long int overflow\n");
-		    return;
-	    }
-        for (i = 0; i < n; i++)
-		    x += size[i] * (1L << (30 * i));
-        if (size < 0)
-		    printf("-");
-        printf("%lu\n", x);
-    }
-    else
-    {
-        printf("Invalid Int Object\n");
-        return;
-    }
+		arr = ((PyLongObject *)(p))->ob_digit;
+		size = ((PyVarObject *)(p))->ob_size;
+		n = abs(size);
+		if (n > 3 || (n == 3 && arr[2] > 15))
+		{
+			printf("C unsigned long int overflow\n");
+			return;
+		}
+		for (i = 0; i < n; i++)
+		x += arr[i] * (1L << (30 * i));
+		if (size < 0)
+		printf("-");
+		printf("%lu\n", x);
+	}
+	else
+	{
+		printf("Invalid Int Object\n");
+		return;
+	}
 }

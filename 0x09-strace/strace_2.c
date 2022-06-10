9e11d0c25d61919ef892d2350cd54a702c7d8850
maroua199525 <2646@holbertonschool.com>
2022-06-10 14:34:28 +0100

    modify code + fix error

--- 0x09-strace/strace_2.c
+++ 0x09-strace/strace_2.c
@@ -1,53 +1,21 @@
 #include "strace.h"
 
 /**
- * syscall_param - print the parametres of a given syscall
- * @registers: struct with the register syscall
- * Return: nothing
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * @env: environ var.
+ * Return: EXIT_SUCCESS or error.
  */
-void syscall_param(struct user_regs_struct registers)
-{
-	unsigned long parameter[6] = {0};
-	size_t nb_params, i = 0;
-
-	parameter[0] = (unsigned long) registers.rdi;
-	parameter[1] = (unsigned long) registers.rsi;
-	parameter[2] = (unsigned long) registers.rdx;
-	parameter[3] = (unsigned long) registers.r10;
-	parameter[4] = (unsigned long) registers.r8;
-	parameter[5] = (unsigned long) registers.r9;
-
-	nb_params = syscalls_64_g[(unsigned long)registers.orig_rax].nb_params;
-
-	while (i < nb_params)
-	{
-		if (i > 0)
-			printf(", ");
-		if (syscalls_64_g[(unsigned long) registers.orig_rax].params[i] == VARARGS)
-			printf("...");
-		else if (syscalls_64_g[(unsigned long) registers.orig_rax].params[i] != VOID)
-			printf("%#lx", parameter[i]);
-		i++;
-	}
-}
-
-
-/**
- * main - use ptrace for getting the syscall name and return value
- * @ac: number of arguments
- * @av: list of arguments
- * @en: list of environ variables
- * Return: 0 on success, 1 otherwise
- */
-int main(int ac, char **av, char **en)
+int main(int argc, char **argv, char **env)
 {
 	pid_t child = 0;
 	int status = 0, flag = 0;
-	struct user_regs_struct registers;
+	struct user_regs_struct regs;
 
-	if (ac < 2)
+	if (argc < 2)
 	{
-		printf("./strace_3 command [args...]\n");
+		printf("./strace_2 command [args...]\n");
 		return (1);
 	}
 	setbuf(stdout, NULL);
@@ -56,7 +24,7 @@ int main(int ac, char **av, char **en)
 	{
 		ptrace(PTRACE_TRACEME, child, NULL, NULL);
 		raise(SIGSTOP);
-		execve(av[1], &(av[1]), en);
+		execve(argv[1], &(argv[1]), env);
 	}
 	else
 	{
@@ -66,17 +34,16 @@ int main(int ac, char **av, char **en)
 		ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 		while (wait(&status) && !WIFEXITED(status))
 		{
-			memset(&registers, 0, sizeof(registers));
-			ptrace(PTRACE_GETREGS, child, NULL, &registers);
+			memset(&regs, 0, sizeof(regs));
+			ptrace(PTRACE_GETREGS, child, NULL, &regs);
 			if (flag)
-				(printf(") = %#lx\n", (unsigned long) registers.rax), flag = 0);
-			if (WSTOPSIG(status) == SIGTRAP && (long) registers.rax == -38)
-				(printf("%s(",
-					(char *) syscalls_64_g[(unsigned long)registers.orig_rax].name),
-				 syscall_param(registers), flag = 1);
+				(printf(" = %#lx\n", (unsigned long) regs.rax), flag = 0);
+			if (WSTOPSIG(status) == SIGTRAP && (long) regs.rax == -38)
+				(printf("%s",
+					(char *) syscalls_64_g[(unsigned long)regs.orig_rax].name), flag = 1);
 			ptrace(PTRACE_SYSCALL, child, NULL, NULL);
 		}
-		printf(") = ?\n");
+		printf(" = ?\n");
 	}
 	return (0);
 }

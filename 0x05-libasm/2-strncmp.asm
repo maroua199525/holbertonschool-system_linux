90a8fab9282df74b18e26745ec0d4111302ceac5
maroua199525 <2646@holbertonstudents.com>
2023-02-06 13:14:25 +0100

    add changes

--- 0x05-libasm/2-strncmp.asm
+++ 0x05-libasm/2-strncmp.asm
@@ -1,50 +1,45 @@
-bits	64		  ; Recognize 64bits asm
-	global	asm_strncmp  ; int strcmp(const char *, const char *, size_t)
-	section	.text		  ; Declare section text
+BITS 64
 
-asm_strncmp:
-	push	rbp		; Add a element to the stack
-	mov 	rbp,	rsp	; Create a backup of the status stack
-	xor	rcx,	rcx	; Create a counter and init to zero
-	cmp	rdx,	0	; if arg3 is equal 0
-	je	it_n_zero	; go function it_n_zero
-	cmp	rdi,	0	; check if arg1 is NULL
-	je	out		; so go out
-	cmp	rsi,	0	; check if arg2 is NULL
-	je	out		; so go out
+global asm_strncmp
 
-loop:
-	mov	al,	[rdi + rcx]	; cpy tmp = arg1[i]
-	mov	r8b,	[rsi + rcx]	; cpy tmp = arg2[i]
-	cmp	al,	0 		; Check if end of string 1
-	je	final_value	  	; so go end of exec
-	cmp	r8b,	0 		; Check if end of string 2
-	je	final_value	  	; so go end of exec
-	cmp	al,	r8b	   	; compare arg1[i], arg2[i]
-	jne	final_value		; if arg1[i] != arg2[i]
-	inc	rcx			; i++
-	cmp	rcx,	rdx		; compare rcx and arg3
-	je	final_value		; go func final_value
-	jmp	loop			; go to loop
-
-final_value:
-	sub	al,	r8b	; al -= r8b
-	movsx	rax,	al	; get le sign of value
-	jmp	out
-
-pos_value:
-	mov	rax,	1	; value = 1
-	jmp	out		; go out
-
-neg_value:
-	mov	rax,	-1	; value = -1
-	jmp	out		; go out
-
-it_n_zero:
-	mov	rax,	0	; value = 0
-	jmp	out		; go out
-
-out:
-	mov	rsp,	rbp	; Get back the status stack
-	pop	rbp		; Delete the element to the stack
-	ret			; return
+section .text
+   asm_strncmp:
+         push rbp
+         mov rbp, rsp
+         push rcx
+         xor rcx, rcx
+   loop_in_bytes_of_sour_and_dest:
+            cmp rdx, 0
+            je return
+            cmp rcx, rdx
+            je compar
+            mov al, [rdi + rcx]
+            mov bl, [rsi + rcx]
+            cmp al, 0
+            je compar
+            cmp bl, 0
+            je compar
+            cmp al , bl
+            jne compar
+            inc rcx
+            jmp loop_in_bytes_of_sour_and_dest
+   compar:
+      cmp al , bl
+      je equal
+      jg greater
+      jl less
+   equal:
+      mov rax , 0
+      jmp return
+   greater:
+      sub al, bl
+      jmp return
+   less:
+      sub bl, al
+      mov al , bl
+      neg rax
+      jmp return
+   return:
+      mov rsp , rbp
+      pop rbp
+      ret

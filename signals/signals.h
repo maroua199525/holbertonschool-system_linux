f9ff72c32d87e9a25fe8177bb8766f1af7926a12
maroua199525 <2646@holbertonstudents.com>
2024-08-21 12:17:45 +0100

    fix header file

--- signals/signals.h
+++ signals/signals.h
@@ -1,14 +1,29 @@
-#ifndef _SIG_H_
-#define _SIG_H_
+#ifndef SIGNALS_H
+#define SIGNALS_H
 
 typedef void (*sighandler_t)(int);
 
-void s_handler(int signalnum);
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/* Functions for Tasks*/
 int handle_signal(void);
+
 void (*current_handler_signal(void))(int);
+void print_hello(int signum);
+void set_print_hello(void);
+
 int handle_sigaction(void);
+
 void (*current_handler_sigaction(void))(int);
+
 int trace_signal_sender(void);
+void sigquit_handler(int signum);
+
 int pid_exist(pid_t pid);
 void all_in_one(void);
 int sigset_init(sigset_t *set, int *signals);
@@ -16,4 +31,4 @@ int signals_block(int *signals);
 int signals_unblock(int *signals);
 int handle_pending(void (*handler)(int));
 
-#endif /* _SIG_H_ */
+#endif /* SIGNALS_H */

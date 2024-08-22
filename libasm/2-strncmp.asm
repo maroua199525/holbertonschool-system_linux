3ee7183b7395f9c6643fa0f99a61bf3fe0a8731b
maroua199525 <2646@holbertonstudents.com>
2024-08-22 09:55:51 +0100

    add tasks

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,42 +1,90 @@
 BITS 64
-	global	asm_strncmp
+
+	global asm_strncmp
+
+
+section .text
+
 
 asm_strncmp:
-	push	rbp
-	mov	rbp, rsp
-	push	rcx
+	push rbp
+	mov rbp, rsp
+	push rcx
 
-loop:
-	movzx 	eax, byte [rdi]
-	movzx 	ecx, byte [rsi]
-	cmp	al, 0x0
-	je	compare
-	cmp	al, cl
-	jne	compare
-	inc	rdi
-	inc	rsi
-	dec	edx
-	jz	success
-	jmp	loop
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	xor rcx, rcx
+	mov r12, rdi
+	mov r13, rsi
+strncmp:
+	cmp rcx, rdx
+	je str_cmp
+	mov r8b, [rdi + rcx]
+	mov r9b, [rsi + rcx]
 
-greater:
-	mov	rax, 0x1
-	jmp	end
+	cmp r8b, byte 0
+	je str_cmp
+	cmp r9b, byte 0
+	je str_cmp
+	cmp r8b, r9b
+	jne str_cmp
+	inc rcx
+	jmp strncmp
+
+str_cmp:
+	cmp r8b, r9b
+	je equal
+	jl less
+	jg greater
+
+equal:
+	mov rax, 0
+	jmp return
 
 less:
-	mov	rax, -0x1
-	jmp	end
+	mov rax, -1
+	jmp return
 
-compare:
-	cmp	al, cl
-	jg	greater
-	jl	less
+greater:
+	mov rax, 1
+	jmp return
 
-success:
-	mov	rax, 0x0
 
-end:
-	pop	rcx
-	mov	rsp, rbp
-	pop	rbp
+;str1_ascii:
+;	mov r10b, [r12]
+;	add byte[r11], r10b
+;	cmp r10b, byte 0
+;	je str2_ascii
+;	inc r12
+;	add byte[r11], r10b
+;	jmp str1_ascii
+;
+;str2_ascii:
+;	mov r12b, [r13]
+;	add byte[r13], r12b
+;	cmp r12b, byte 0
+;	je ret_ascii
+;	inc r13
+;	add byte[r13], r12b
+;	jmp str2_ascii
+;
+;ret_ascii:
+;       sub r11, r13
+;	jmp return
+;strcmp returns the ascii difference between s1 and s2 which is different from the current version of my program
+
+return:
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rcx
+	mov rsp, rbp
+	pop rbp
 	ret

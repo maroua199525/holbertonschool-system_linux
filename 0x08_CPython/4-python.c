b8a5b498b86651c35aeb87d7e96b79604315078f
maroua199525 <2646@holbertonstudents.com>
2024-08-22 11:13:04 +0100

    fix task

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -1,33 +1,22 @@
 #include <Python.h>
 
 /**
- *print_python_string - function to print about list
- *@p: pointer refer to object of python
- *Return:
- */
+* print_python_string - Print information about a Python String object.
+*
+* This function prints information about a Python String object.
+*
+* @p: A pointer to the Python String object.
+*/
 void print_python_string(PyObject *p)
 {
-	wchar_t *buffer = NULL;
-
-	printf("[.] string object info\n");
-
-	if (!PyUnicode_Check(p) || !((PyASCIIObject *)p)->state.ready)
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
-
-	printf("  type: %s%s\n",
-	       ((PyASCIIObject *)p)->state.compact ? "compact " : "",
-	       ((PyASCIIObject *)p)->state.ascii ? "ascii" : "unicode object");
-
-	printf("  length: %li\n",
-	       ((PyASCIIObject *)(p))->length);
-	buffer = PyUnicode_AsWideCharString(p, NULL);
-	if (buffer != NULL)
-	{
-		printf("  value: %ls\n", buffer);
-
-		PyMem_Free(buffer);
-	}
+    printf("[.] string object info\n");
+    if (PyUnicode_CheckExact(p) == 0)
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+    printf("  type: %s\n",
+        PyUnicode_IS_COMPACT_ASCII(p) ? "compact ascii" : "compact unicode object");
+    printf("  length: %zi\n", PyUnicode_GetLength(p)); // Updated to PyUnicode_GetLength
+    printf("  value: %s\n", PyUnicode_AsUTF8(p));
 }

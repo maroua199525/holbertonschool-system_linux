2898f67bae78a4f8a01b5cd9a45aecba6775ebc3
maroua199525 <2646@holbertonstudents.com>
2024-09-30 12:15:10 +0100

    add file to tast

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -3,24 +3,26 @@ section .text
 global asm_strncmp
 
 asm_strncmp:
-    mov rax, 0            ; Clear rax (will store the result)
-    mov rcx, 0            ; Clear rcx (counter)
+    xor rax, rax            ; Clear rax (will store the result)
+    xor rcx, rcx            ; Clear rcx (counter)
 
 loop:
     mov bl, BYTE [rdi + rcx]    ; Load byte from first string into BL
     mov bh, BYTE [rsi + rcx]    ; Load byte from second string into BH
     cmp bl, bh                  ; Compare the two characters
-    jne diff                    ; If they differ, go to 'diff'
+    jne diff                    ; If they differ, jump to 'diff'
     inc rcx                     ; Increment counter
-    cmp rcx, rdx                ; If we've reached the specified length (rdx)
-    je out                      ; Return 0 if strings are equal up to rdx
-    test bl, bl                 ; Check for null terminator in the first string
-    je out                      ; If we hit null terminator, exit
-    jmp loop                    ; Repeat loop
+    cmp rcx, rdx                ; Check if we've reached the specified length
+    je out                      ; If yes, exit with result 0
+    test bl, bl                 ; Check for null terminator in first string
+    je out                      ; Exit if null terminator (strings are equal up to that point)
+    jmp loop                    ; Repeat the loop
 
 diff:
     sub rax, rbx                ; Calculate difference: rax = BL - BH
-    ret                         ; Return the difference
+    mov al, bl                  ; Place the value of the differing character from the first string in rax
+    sub al, bh                  ; Subtract the corresponding character from the second string
+    ret                         ; Return the difference (rax)
 
 out:
-    ret   
+    ret                         ; Return 0 (strings are equal)

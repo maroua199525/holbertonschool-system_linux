0da8f3dfa1dadeb990263e628fad644b4861d5d9
maroua199525 <2646@holbertonstudents.com>
2024-09-30 11:40:33 +0100

    add file to tast

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -1,29 +1,24 @@
 BITS 64
-		section .text
-		global asm_strncmp
+section .text
+global asm_strncmp
+
 asm_strncmp:
-        mov rax, 0
-	mov rcx, 0
+    mov rax, 0            ; Clear rax (return value)
+    mov rcx, 0            ; Initialize counter
+
 loop:
-        mov BL, BYTE [rdi + rcx]    ; store first arg char in BL
-        mov BH, BYTE [rsi + rcx]    ; store second arg char in BH
-        cmp BL, BH	; compare them
-        jnz diff	; when not zero return 1 or -1
-        inc rcx		; dec counter
-        cmp rcx, rdx	; when rdx is at zero
-        je out		; return 0
-        jmp loop	; loop again
+    mov BL, BYTE [rdi + rcx]    ; Load byte from first string into BL
+    mov BH, BYTE [rsi + rcx]    ; Load byte from second string into BH
+    cmp BL, BH                  ; Compare the two characters
+    jnz diff                    ; If they differ, go to 'diff'
+    inc rcx                     ; Increment the counter
+    cmp rcx, rdx                ; Check if we've reached the specified length
+    je out                      ; If yes, return 0
+    jmp loop                    ; Repeat the loop
+
 diff:
-	sub rdi + rcx, rsi + rcx
-	cmp rdi + rcx, 0
-	jg greater
-	jl less
-        je out
-greater:
-	mov rax, 1
-	jmp out
-less:
-	mov rax, -1
-	jmp out
+    sub rax, rbx                ; Compute difference between characters (BL - BH)
+    ret                         ; Return the difference in rax
+
 out:
-        ret
+    ret                         ; Return 0 if strings are equal

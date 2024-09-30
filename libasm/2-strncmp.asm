06b3a21c704586759f34bc57a0ec9a977a9f44c3
maroua199525 <2646@holbertonstudents.com>
2024-09-30 12:24:49 +0100

    add file to tast

--- libasm/2-strncmp.asm
+++ libasm/2-strncmp.asm
@@ -3,26 +3,34 @@ section .text
 global asm_strncmp
 
 asm_strncmp:
-    xor rax, rax            ; Clear rax (will store the result)
-    xor rcx, rcx            ; Clear rcx (counter)
+    push rbp                     ; Save base pointer
+    mov rbp, rsp                 ; Set base pointer
+    push rcx                     ; Save rcx (for later use)
+
+    xor rax, rax                 ; Clear rax (this will hold the result)
+    xor rcx, rcx                 ; Clear rcx (counter for looping)
 
 loop:
-    mov bl, BYTE [rdi + rcx]    ; Load byte from first string into BL
-    mov bh, BYTE [rsi + rcx]    ; Load byte from second string into BH
-    cmp bl, bh                  ; Compare the two characters
-    jne diff                    ; If they differ, jump to 'diff'
-    inc rcx                     ; Increment counter
-    cmp rcx, rdx                ; Check if we've reached the specified length
-    je out                      ; If yes, exit with result 0
-    test bl, bl                 ; Check for null terminator in first string
-    je out                      ; Exit if null terminator (strings are equal up to that point)
-    jmp loop                    ; Repeat the loop
+    movzx bl, BYTE [rdi + rcx]    ; Load byte from first string into BL
+    movzx bh, BYTE [rsi + rcx]    ; Load byte from second string into BH
+    cmp bl, bh                    ; Compare the characters
+    jne diff                      ; If they differ, jump to 'diff'
+    test bl, bl                   ; Check if null terminator reached in first string
+    je out                        ; If null, strings are equal up to this point
+    inc rcx                       ; Increment counter
+    cmp rcx, rdx                  ; Check if we've compared rdx bytes
+    je out                        ; If yes, the strings are equal up to rdx bytes
+    jmp loop                      ; Repeat the loop
 
 diff:
-    sub rax, rbx                ; Calculate difference: rax = BL - BH
-    mov al, bl                  ; Place the value of the differing character from the first string in rax
-    sub al, bh                  ; Subtract the corresponding character from the second string
-    ret                         ; Return the difference (rax)
+    sub eax, ebx                  ; Calculate difference: eax = bl - bh
+    jmp end                       ; Return the difference
 
 out:
-    ret                         ; Return 0 (strings are equal)
+    xor eax, eax                  ; Set eax to 0 (strings are equal)
+
+end:
+    pop rcx                       ; Restore rcx
+    mov rsp, rbp                  ; Restore stack pointer
+    pop rbp                       ; Restore base pointer
+    ret                           ; Return

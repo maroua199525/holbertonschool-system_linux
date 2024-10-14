c3fc3564093fdc54501637fcdf8d4d00fc4294c6
maroua199525 <2646@holbertonstudents.com>
2024-10-14 11:48:07 +0100

    add task to test

--- signals/10-pid_exist.c
+++ signals/10-pid_exist.c
@@ -1,12 +1,12 @@
-#include <sys/types.h>
 #include <signal.h>
 
 /**
-* pid_exist - check PID
-* @pid: pid
-* Return: 0, 1
-*/
+ * pid_exist - check if a pid is running
+ * @pid: pid to check
+ *
+ * Return: 1 if exists, 0 if not
+ */
 int pid_exist(pid_t pid)
 {
-	return (kill(pid, 0) + 1);
+	return (kill(pid, 0));
 }

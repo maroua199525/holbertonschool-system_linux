1aeefc5c6a4288abc4b6542cadadf2edd5be50ed
maroua199525 <2646@holbertonstudents.com>
2024-11-15 13:05:52 +0100

    add task

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -4,39 +4,27 @@
 
 void print_python_string(PyObject *po)
 {
-    setbuf(stdout, NULL);
-    printf("[.] string object info\n");
+	PyASCIIObject *pao = NULL;
 
-    // Validate input object
-    if (!po || !PyUnicode_Check(po))
-    {
-        printf("  [ERROR] Invalid String Object\n");
-        return;
-    }
+	setbuf(stdout, NULL);
+	printf("[.] string object info\n");
 
-    // Get string length
-    Py_ssize_t length = PyUnicode_GET_LENGTH(po);
-    printf("  length: %zd\n", length);
+	if (!po || po->ob_type != &PyUnicode_Type)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
 
-    // Determine string type and value
-    if (PyUnicode_IS_COMPACT_ASCII(po))
-    {
-        printf("  type: compact ascii\n");
-        printf("  value: %s\n", PyUnicode_1BYTE_DATA(po));
-    }
-    else
-    {
-        printf("  type: compact unicode object\n");
-
-        // Print the Unicode string as UTF-8
-        const char *utf8_value = PyUnicode_AsUTF8(po);
-        if (utf8_value)
-        {
-            printf("  value: %s\n", utf8_value);
-        }
-        else
-        {
-            printf("  [ERROR] Unable to convert to UTF-8\n");
-        }
-    }
+	pao = (PyASCIIObject *)po;
+	if (pao->state.ascii)
+	{
+		printf("  type: compact ascii\n");
+		printf("  value: %s\n", PyUnicode_1BYTE_DATA(po));
+	}
+    printf("  length: %lu\n", pao->length);
+	else
+	{
+		printf("  type: compact unicode object\n");
+		printf("  value: %s\n", PyUnicode_AsUTF8(p));
+	}
 }

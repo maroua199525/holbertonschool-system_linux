29aa341f3a81b221f95e637dcf1e0bdbbb4358c9
maroua199525 <2646@holbertonstudents.com>
2024-11-18 12:35:07 +0100

    add task to test

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -1,37 +1,38 @@
 #include <Python.h>
 
-
 /**
- *print_python_int - function to print integer information
- *@p: pointer to  a list of python
- *Return: Nothing
- */
+* print_python_int - Print information about a Python Int object.
+*
+* This function prints information about a Python Int object
+*
+* @p: A pointer to the Python Int object.
+*/
 void print_python_int(PyObject *p)
 {
-	unsigned long x = 0;
-		digit *arr;
-	Py_ssize_t size, i, n;
+	Py_ssize_t size, i;
+	PyLongObject *long_object;
+	unsigned long result = 0;
 
-
-	if (PyLong_Check(p))
-	{
-		arr = ((PyLongObject *)(p))->ob_digit;
-		size = ((PyVarObject *)(p))->ob_size;
-		n = abs(size);
-		if (n > 3 || (n == 3 && arr[2] > 15))
-		{
-			printf("C unsigned long int overflow\n");
-			return;
-		}
-		for (i = 0; i < n; i++)
-		x += arr[i] * (1L << (30 * i));
-		if (size < 0)
-		printf("-");
-		printf("%lu\n", x);
-	}
-	else
+	if (PyLong_Check(p) == 0)
 	{
 		printf("Invalid Int Object\n");
 		return;
 	}
+	long_object = (PyLongObject *)p;
+	size = long_object->ob_base.ob_size;
+	if (size < 0)
+	{
+		printf("-");
+		size = -size;
+	}
+	if (size > 3 || (size == 3 && long_object->ob_digit[2] > 15))
+	{
+		printf("C unsigned long int overflow\n");
+		return;
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		result = result * (1 << PyLong_SHIFT) + long_object->ob_digit[i];
+	}
+	printf("%lu\n", result);
 }

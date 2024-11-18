945e90a7627b8ff4bbd7a073237ca7f84184c1bf
maroua199525 <2646@holbertonstudents.com>
2024-11-18 12:41:09 +0100

    add task to test

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -10,30 +10,30 @@
 void print_python_int(PyObject *p)
 {
 	Py_ssize_t size, i;
+	PyLongObject *long_object;
 	unsigned long result = 0;
-	PyLongObject *long;
 
 	if (PyLong_Check(p) == 0)
 	{
 		printf("Invalid Int Object\n");
 		return;
 	}
-	long = (PyLongObject *)p;
-	size = long->ob_base.ob_size;
+	long_object = (PyLongObject *)p;
+	size = long_object->ob_base.ob_size;
 	if (size < 0)
 	{
 		printf("-");
 		size = -size;
 	}
-	if (size > 3 || (size == 3 && long->ob_digit[2] > 15))
+	if (size > 3 || (size == 3 && long_object->ob_digit[2] > 15))
 	{
 		printf("C unsigned long int overflow\n");
 		return;
 	}
-	i = size - 1
+	i = size - 1;
 	while (i >= 0)
 	{
-		result = result * (1 << PyLong_SHIFT) + long->ob_digit[i];
+		result = result * (1 << PyLong_SHIFT) + long_object->ob_digit[i];
 		i--;
 	}
 	printf("%lu\n", result);

9ba16ece461fd6cda22323d36d96107e2c13f840
maroua199525 <2646@holbertonstudents.com>
2024-11-18 14:47:49 +0100

    add task to test

--- 0x08_CPython/5-python.c
+++ 0x08_CPython/5-python.c
@@ -7,36 +7,41 @@
 
 void print_python_int(PyObject *po)
 {
-	PyVarObject *pvo = (PyVarObject *)po;
-	PyLongObject *plo = (PyLongObject *)po;
-	ssize_t index, size, is_neg;
-	unsigned long result, buffer;
-	int shift;
+    PyVarObject *pvo = (PyVarObject *)po;
+    PyLongObject *plo = (PyLongObject *)po;
+    ssize_t index, size, is_neg;
+    unsigned long result = 0, buffer;
+    int shift;
 
-	setbuf(stdout, NULL);
+    setbuf(stdout, NULL);
 
-	if (!PyLong_Check(po))
-	{
-		printf("Invalid Int Object\n");
-		return;
-	}
-	size = pvo->ob_size;
-	is_neg = size < 0;
-	size = is_neg ? -size : size;
+    if (!PyLong_Check(po))
+    {
+        printf("Invalid Int Object\n");
+        return;
+    }
 
-	if (size > 3 || (size == 3 && plo->ob_digit[2] < 15))
-	{
-		printf("C unsigned long int overflow\n");
-		return;
-	}
-	for (index = size -1; index > 0; index--)
-	{
-		shift = PyLong_SHIFT * index;
-		buffer = ((unsigned long)plo->ob_digit[index]) * (1UL << (shift));
-		result += buffer;
-	}
+    size = pvo->ob_size;
+    is_neg = size < 0;
+    size = is_neg ? -size : size;
 
-	if (is_neg)
-		printf("-");
-	printf("%lu\n", result);
+    // Overflow check
+    if (size > 3 || (size == 3 && plo->ob_digit[2] > 15))
+    {
+        printf("C unsigned long int overflow\n");
+        return;
+    }
+
+    // Reconstruct the number
+    for (index = 0; index < size; index++)
+    {
+        shift = (size - index - 1) * PyLong_SHIFT;
+        buffer = ((unsigned long)plo->ob_digit[index]) * (1UL << shift);
+        result += buffer;
+    }
+
+    // Print the number with a negative sign if needed
+    if (is_neg)
+        printf("-");
+    printf("%lu\n", result);
 }

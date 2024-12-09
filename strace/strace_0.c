294d6c48b0c981e4e9dceed98ec94dc562da38d8
maroua199525 <2646@holbertonstudents.com>
2024-12-09 09:58:10 +0100

    add to test

--- strace/strace_0.c
+++ strace/strace_0.c
@@ -1,93 +1,44 @@
-#include "strace.h"
-
-void _trace_child(char **argv, char **env);
-void _trace_parent(pid_t child_pid);
-int _await_syscall(pid_t child_pid);
+#include "syscalls.h"
 
 /**
- * main - entry point
+ * main - traces a process and prints system call numbers as they're called
  * @argc: argument count
- * @argv: argument vector
- * @env: environ
- * Return: EXIT_SUCCESS or error.
- */
-int main(int argc, char **argv, char **env)
+ * @argv: argument array
+ * @envp: environment parameters
+ * Return: 0 on success | 1 on failure (not enough arguments)
+ **/
+int main(int argc, char *argv[], char *envp[])
 {
-	pid_t child_pid;
+	int print, status;
+	struct user_regs_struct regs;
+	pid_t pid;
 
 	if (argc < 2)
 	{
-		printf("Usage: %s command [args...]\n", argv[0]);
-		return (EXIT_FAILURE);
+		fprintf(stderr, "Usage: %s <full_path> [path_args]\n", argv[0]);
+		return (1);
 	}
+
 	setbuf(stdout, NULL);
-	child_pid = fork();
-	if (child_pid == -1)
+	pid = fork();
+
+	if (pid == 0)
 	{
-		dprintf(STDERR_FILENO, "Fork failed: %d\n", errno);
-		exit(-1);
+		printf("59\n"); /* execve syscall number */
+		ptrace(PTRACE_TRACEME, pid, NULL, NULL);
+		execve(argv[1], argv + 1, envp);
 	}
-	else if (!child_pid)
-		_trace_child(argv, env);
 	else
-		_trace_parent(child_pid);
+	{
+		for (status = 1, print = 0; !WIFEXITED(status); print ^= 1)
+		{
+			ptrace(PT_SYSCALL, pid, NULL, NULL);
+			wait(&status);
+			ptrace(PT_GETREGS, pid, NULL, &regs);
+			if (print)
+				printf("%lu\n", (size_t)regs.orig_rax);
+		}
+	}
+
 	return (0);
-}
-/**
- * _trace_child - traces child process
- * @argv: argument vector for execve
- * @env: environ for execve
- */
-void _trace_child(char **argv, char **env)
-{
-	ptrace(PTRACE_TRACEME, 0, 0, 0);
-	kill(getpid(), SIGSTOP);
-	if (execve(argv[1], argv + 1, env) == -1)
-	{
-		dprintf(STDERR_FILENO, "Exec failed: %d\n", errno);
-		exit(-1);
-	}
-}
-
-/**
- * _trace_parent - calls made by tracing parent
- * @child_pid: pid of child to trace
- */
-void _trace_parent(pid_t child_pid)
-{
-	int status;
-	struct user_regs_struct registers;
-
-	waitpid(child_pid, &status, 0);
-	ptrace(PTRACE_SETOPTIONS, child_pid, 0, PTRACE_O_TRACESYSGOOD);
-	while (1)
-	{
-		if (_await_syscall(child_pid))
-			break;
-		memset(&registers, 0, sizeof(registers));
-		ptrace(PTRACE_GETREGS, child_pid, 0, &registers);
-		printf("%lu\n", (long)registers.orig_rax);
-		if (_await_syscall(child_pid))
-			break;
-	}
-}
-
-/**
- * _await_syscall - waits for a syscall
- * @child_pid: pid of process to await
- * Return: 0 if child stopped, 1 if exited
- */
-int _await_syscall(pid_t child_pid)
-{
-	int status;
-
-	while (1)
-	{
-		ptrace(PTRACE_SYSCALL, child_pid, 0, 0);
-		waitpid(child_pid, &status, 0);
-		if (WIFSTOPPED(status) && WSTOPSIG(status) & 0x80)
-			return (0);
-		if (WIFEXITED(status))
-			return (1);
-	}
 }

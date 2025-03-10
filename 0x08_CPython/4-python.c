48f0a59c0011a8c51e2264d6e08d3e25371a8328
maroua199525 <2646@holbertonstudents.com>
2025-03-10 12:50:56 +0100

    add to test

--- 0x08_CPython/4-python.c
+++ 0x08_CPython/4-python.c
@@ -1,114 +1,29 @@
 #include <Python.h>
+#include <stdio.h>
 
-
-
-void ucs_to_utf8(void *pos, int kind, Py_ssize_t length);
-
-
-/**
- * print_python_string - func
- * @p: PyObject
- */
-void print_python_string(PyObject *p)
+void print_python_string(PyObject *po)
 {
-	char	*type = NULL,
-			*a = "comapct ascii",
-			*b = "legacy ascii",
-			*c = "comapct unicode object",
-			*d = "legacy string",
-			*pos = NULL;
-	PyASCIIObject *ascii_ob = NULL;
-	PyCompactUnicodeObject *uni_ob = NULL;
+    if (!PyUnicode_Check(po))
+    {
+        fprintf(stderr, "Invalid Unicode object\n");
+        return;
+    }
 
-	printf("[.] string object info\n");
-	if (!PyUnicode_Check(p))
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
-	uni_ob = (PyCompactUnicodeObject *)p;
-	ascii_ob = (PyASCIIObject *)&uni_ob->_base;
-	if (ascii_ob->state.ascii)
-	{
-		type = (ascii_ob->state.compact) ? a : b;
-		pos = (char *)&ascii_ob->state;
-		pos += sizeof(ascii_ob->state) + 4;
-		printf("  type: %s\n", type);
-		printf("  length: %ld\n", ascii_ob->length);
-		printf("  value: %s\n", pos);
-	}
-	else
-	{
-		type = (ascii_ob->state.compact) ? c : d;
-		pos = (char *)&uni_ob->utf8;
-		printf("  type: %s\n", type);
-		printf("  length: %ld\n", ascii_ob->length);
-		printf("  value: ");
-		fflush(NULL);
-		ucs_to_utf8(pos, ascii_ob->state.kind, ascii_ob->length);
-		fflush(NULL);
-		printf("\n");
-	}
-}
+    printf("[.] string object info\n");
 
+    /* Get Unicode kind */
+    Py_ssize_t length = PyUnicode_GET_LENGTH(po);
+    const char *utf8_str = PyUnicode_AsUTF8(po);
 
-/**
- * ucs_to_utf8 - func
- * @pos: pointer to data
- * @kind: kind of ucs
- * @length:	data length
- */
-void ucs_to_utf8(void *pos, int kind, Py_ssize_t length)
-{
-	Py_UCS1 *ucs1 = NULL;
-	Py_UCS2 *ucs2 = NULL;
-	Py_UCS4 *ucs4 = NULL;
-	unsigned char c = 0;
-	unsigned int i = 0;
+    /* Print the type */
+    if (PyUnicode_IS_COMPACT_ASCII(po))
+        printf("  type: compact ascii\n");
+    else
+        printf("  type: compact unicode object\n");
 
-	if (kind == PyUnicode_1BYTE_KIND)
-	{
-		ucs1 = (Py_UCS1 *)pos;
-		for (int j = 0; j < length; j++)
-		{
-			c = ucs1[j];
-			if (c <= 0x7f)
-				printf("%c", c);
-			else
-				printf("%c%c", 0xc0 | (c >> 6), 0x80 | (c & 0x3f));
-		}
-	}
-	if (kind == PyUnicode_2BYTE_KIND)
-	{
-		ucs2 = (Py_UCS2 *)pos;
-		for (Py_ssize_t j = 0; j < length; j++)
-		{
-			i = ucs2[j];
-			if (i <= 0x7f)
-				putchar(i);
-			else if (i <= 0x7ff)
-				printf("%c%c", 0xc0 | (i >> 6), 0x80 | (i & 0x3f));
-			else
-				printf("%c%c%c",
-				0xe0 | (i >> 12), 0x80 | ((i >> 6) & 0x3f), 0x80 | (i & 0x3f));
-		}
-	}
-	else if (kind == PyUnicode_4BYTE_KIND)
-	{
-		ucs4 = (Py_UCS4 *)pos;
-		for (Py_ssize_t j = 0; j < length; j++)
-		{
-			i = ucs4[j];
-			if (i <= 0x7f)
-				putchar(i);
-			else if (i <= 0x7ff)
-				printf("%c%c", 0xc0 | (i >> 6), 0x80 | (i & 0x3f));
-			else if (i <= 0xffff)
-				printf("%c%c%c",
-				0xe0 | (i >> 12), 0x80 | ((i >> 6) & 0x3f), 0x80 | (i & 0x3f));
-			else
-				printf("%c%c%c%c", 0xf0 | (i >> 18), 0x80 | ((i >> 12) & 0x3f),
-				0x80 | ((i >> 6) & 0x3f), 0x80 | (i & 0x3f));
-		}
-	}
+    /* Print the length */
+    printf("  length: %zd\n", length);
+
+    /* Print the UTF-8 value */
+    printf("  value: %s\n", utf8_str);
 }
